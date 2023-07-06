483cc16d2ce1a3c5dccf7c468a783ce68aedd68d
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-07-06 14:16:54 -0500

    Refactor Cypress details tests

--- cypress/e2e/details.cy.ts
+++ cypress/e2e/details.cy.ts
@@ -4,6 +4,7 @@ describe("Details View", () => {
       statusCode: 202,
     })
     cy.intercept("GET", "https://closet-manager-be.herokuapp.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBEdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f992d19ee8a075a2504c274ce72a7b33b98e7851/orange-top.avif", {})
+    cy.intercept("GET", "https://closet-manager-be.herokuapp.com/api/v1/users/1/items/find_all?", {fixture: "closet"})
     cy.intercept("GET", "https://closet-manager-be.herokuapp.com/api/v1/users/1/items", {fixture: "closet"})
     cy.intercept("GET", "https://closet-manager-be.herokuapp.com/api/v1/users/1/items/1", {fixture: "details"})
     cy.intercept("DELETE", "https://closet-manager-be.herokuapp.com/api/v1/users/1/items/1", {fixture: "deletedItem"})
@@ -18,79 +19,67 @@ describe("Details View", () => {
   });
   
   it("Should display the item's color, size, season, and type", () => {
-    cy.get("p[class='item-details']").eq(0).should("have.text", "orange");
-    cy.get("p[class='item-details']").eq(1).should("have.text", "summer");
-    cy.get("p[class='item-details']").eq(2).should("have.text", "tops");
-    cy.get("p[class='item-details']").eq(3).should("have.text", "size small");
+    cy.get("p[class='item-details']").eq(0).should("have.text", "red");
+    cy.get("p[class='item-details']").eq(1).should("have.text", "Size Large");
+    cy.get("p[class='item-details']").eq(2).should("have.text", "fall");
   });
 
   it("Should redirect the user to an edit page when the item's color is clicked", () => {
-    cy.get("p[class='item-details']").eq(0).should("be.visible");
-    cy.get("p[class='item-details']").eq(0).click();
-    cy.on("url:changed", (newUrl) => {
-      expect(newUrl).to.contain("/edit/1");
+     cy.get("p[class='item-details']").eq(0).should("be.visible");
+     cy.get("p[class='item-details']").eq(0).click();
+     cy.on("url:changed", (newUrl) => {
+       expect(newUrl).to.contain("/edit/1");
     });
   });
   
   it("Should redirect the user to an edit page when the item's season is clicked", () => {
-    cy.get("p[class='item-details']").eq(1).should("be.visible");
-    cy.get("p[class='item-details']").eq(1).click();
-    cy.on("url:changed", (newUrl) => {
-      expect(newUrl).to.contain("/edit/1");
-    });
-  });
-  
-  it("Should redirect the user to an edit page when the item's type is clicked", () => {
-    cy.get("p[class='item-details']").eq(2).should("be.visible");
-    cy.get("p[class='item-details']").eq(2).click();
-    cy.on("url:changed", (newUrl) => {
-      expect(newUrl).to.contain("/edit/1");
-    });
+     cy.get("p[class='item-details']").eq(1).should("be.visible");
+     cy.get("p[class='item-details']").eq(1).click();
+     cy.on("url:changed", (newUrl) => {
+       expect(newUrl).to.contain("/edit/1");
+     });
   });
   
   it("Should redirect the user to an edit page when the item's size is clicked", () => {
-    cy.get("p[class='item-details']").eq(3).should("be.visible");
-    cy.get("p[class='item-details']").eq(3).click();
-    cy.on("url:changed", (newUrl) => {
-      expect(newUrl).to.contain("/edit/1");
-    });
+     cy.get("p[class='item-details']").eq(1).should("be.visible");
+     cy.get("p[class='item-details']").eq(1).click();
+     cy.on("url:changed", (newUrl) => {
+       expect(newUrl).to.contain("/edit/1");
+     });
   });
 
   it("Should display an image of the item", () => {
-    cy.get("img[class='details-image']").should("be.visible");
-    cy.get("img[class='details-image']").should("have.attr", "src").should("eq", "https://closet-manager-be.herokuapp.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBEdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f992d19ee8a075a2504c274ce72a7b33b98e7851/orange-top.avif") 
+     cy.get("img[class='item-details-image']").should("be.visible");
+     cy.get("img[class='item-details-image']").should("have.attr", "src").should("eq", "../red-top.avif") 
   });
 
   it("Should display an item's notes", () => {
-    cy.get("h2[class='item-notes-header']").should("have.text", "Notes");
-    cy.get("p[class='item-notes']").should("have.text", "Wash on cold only");
+     cy.get("p[class='item-notes-header']").should("have.text", "Notes");
+     cy.get("p[class='item-notes']").should("have.text", "Hand wash only");
   });
 
   it("Should have an edit button, an add to list button, and a delete button", () => {
-    cy.get("button[class='details-edit-button']").should("have.text", "Edit");
-    cy.get('.add-to-list-container > button').should("have.text", "Add to List");
-    cy.get("button[class='details-delete-button']").should("have.text", "Delete");
+     cy.get("div[class='toggle-icons'] > i").should("have.length", 2)
   });
 
   it("Should redirect the user to an edit page when the edit button is clicked", () => {
-    cy.get("button[class='details-edit-button']").click();
-    cy.on("url:changed", (newUrl) => {
-      expect(newUrl).to.contain("/edit/1");
-    });
+     cy.get("[class='edit-link']").click();
+     cy.on("url:changed", (newUrl) => {
+       expect(newUrl).to.contain("/edit/1");
+     });
   });
 
   it("Should delete the item when the user clicks the delete button and redirect the user to the Closet view", () => {
-    cy.get("button[class='details-delete-button']").click();
-    cy.on("url:changed", (newUrl) => {
-      expect(newUrl).to.contain("/");
-    });
+     cy.get("div[class='toggle-icons'] > i").eq(1).click()
+     cy.get("div[class='details-delete-container']").click()
+     cy.on("url:changed", (newUrl) => {
+       expect(newUrl).to.contain("/");
+     });
   });
 
   it("Should be able to add an item to a list", () => {
-    cy.get('select').select('Goodwill').get('button').eq(1).click()
-  });
-
-  it("Should be able to add an item to a date on the calendar", () => {
-    cy.get('p[class="cal-text"]').should("have.text", "Add to Calendar:")
+    cy.get("div[class='toggle-icons'] > i").eq(0).click()
+    cy.get("div[class='list-input-container']").eq(0).get("input").eq(0).click()
+    cy.get("[class='add-to-list-btn']").click()
   });
 });
